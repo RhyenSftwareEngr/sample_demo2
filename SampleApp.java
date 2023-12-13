01f92062f97ef0b4c7f5f48e612921bb567624eb
PainSustain <89228314+RhyenSftwareEngr@users.noreply.github.com>
2023-12-13 10:50:23 +0800

    Create SampleApp.java

---
+++ SampleApp.java
@@ -0,0 +1,17 @@
+public class SampleApp {
+
+    public static void main(String[] args) {
+        String password = "mySecretPassword";
+        authenticate(password);
+    }
+
+    public static void authenticate(String enteredPassword) {
+        String storedPassword = "mySecretPassword";
+
+        if (enteredPassword.equals(storedPassword)) {
+            System.out.println("Authentication successful!");
+        } else {
+            System.out.println("Authentication failed!");
+        }
+    }
+}
